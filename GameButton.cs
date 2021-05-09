6e8b7cbb2b4eb7e4902792ecdf47fb6807bd5fb7
Nikita <nikitabaranov200021@gmail.com>
2021-05-10 01:47:21 +1000

    Add check lines

--- GameButton.cs
+++ GameButton.cs
@@ -5,8 +5,9 @@ using System.Windows.Controls;
 
 namespace TestTaskGF
 {
+    public enum Figure : int { Triangle = '\u25B2', Square = '\u25A0', Cicle = '\u25CF', Diamond = '\u25C6', Star = '\u2605' }
     class GameButton : Button
     {
-
+        public Figure CurrentFigure { get; set; }
     }
 }
--- GameWindow.xaml.cs
+++ GameWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,14 +19,27 @@ namespace TestTaskGF
     public partial class GameWindow : Window
     {
         const int CELLSCOUNT = 8;
+        const int MINMATCHCOUNT = 3;
         Random rand;
 
-        enum Figure : int { Triangle = '\u25B2', Square = '\u25A0', Cicle = '\u25CF', Diamond = '\u25C6', Star = '\u2605'}
         public GameWindow()
         {
             InitializeComponent();
+            ConfigureGame();         
+        }
+
+        private void ConfigureGame()
+        {
             rand = new Random();
-            InitializeCells();          
+            InitializeCells();
+            for (int i = 0; i < CELLSCOUNT; i++)
+            {
+                foreach (var pair in CheckLines(i, i))
+                {
+                    gameGrid.Children.Cast<GameButton>()
+                        .First(p => Grid.GetRow(p) == pair.Item1 && Grid.GetColumn(p) == pair.Item2).Background = Brushes.Red;
+                }
+            }
         }
 
         private void InitializeCells()
@@ -35,13 +49,14 @@ namespace TestTaskGF
                 for (int j = 0; j < CELLSCOUNT; j++)
                 {
                     var btn = new GameButton();
-                    var picture = (Figure)Enum.GetValues(typeof(Figure)).GetValue(rand.Next(0, Enum.GetValues(typeof(Figure)).Length));
-                    btn.Content = (char)picture;
+                    btn.CurrentFigure = (Figure)Enum.GetValues(typeof(Figure)).GetValue(rand.Next(0, Enum.GetValues(typeof(Figure)).Length));
+                    btn.Content = (char)btn.CurrentFigure;
                     btn.Width = 40;
                     btn.Height = 40;
                     btn.FontSize = 30;
                     Grid.SetRow(btn, i);
                     Grid.SetColumn(btn, j);
+                    btn.Click += Button_Click;
                     gameGrid.Children.Add(btn);
                 }
             }
@@ -49,12 +64,85 @@ namespace TestTaskGF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-          
+            var btn = (GameButton)(sender);
+            int i = Grid.GetRow(btn);
+            int j = Grid.GetColumn(btn);
+            var newBtn = gameGrid.Children.Cast<GameButton>()
+                .First(p => Grid.GetRow(p) == i && Grid.GetColumn(p) == j);
+        }
+
+        private HashSet<Tuple<int, int>> CheckLines(int i, int j)
+        {
+            HashSet<Tuple<int, int>> matchCoords = new HashSet<Tuple<int, int>>();
+            HashSet<Tuple<int, int>> tempMatchCoords = new HashSet<Tuple<int, int>>();
+            for (int colJ = 0; colJ < CELLSCOUNT - 1; colJ++)
+            {
+                if (gameGrid.Children.Cast<GameButton>().First(p => Grid.GetRow(p) == i && Grid.GetColumn(p) == colJ).CurrentFigure ==
+                    gameGrid.Children.Cast<GameButton>().First(p => Grid.GetRow(p) == i && Grid.GetColumn(p) == colJ + 1).CurrentFigure)
+                {
+                    if (tempMatchCoords.Count == 0)
+                    {
+                        tempMatchCoords.Add(new Tuple<int, int>(i, colJ));
+                    }
+                    tempMatchCoords.Add(new Tuple<int, int>(i, colJ + 1));
+                }
+                else if (tempMatchCoords.Count >= MINMATCHCOUNT)
+                {
+                    foreach (var el in tempMatchCoords)
+                        matchCoords.Add(el);
+                    tempMatchCoords.Clear();
+                }
+                else tempMatchCoords.Clear();
+            }
+            if (tempMatchCoords.Count >= MINMATCHCOUNT)
+            {
+                foreach (var el in tempMatchCoords)
+                    matchCoords.Add(el);
+            }
+            tempMatchCoords.Clear();
+            for (int rowI = 0; rowI < CELLSCOUNT - 1; rowI++)
+            {
+                if (gameGrid.Children.Cast<GameButton>().First(p => Grid.GetRow(p) == rowI && Grid.GetColumn(p) == j).CurrentFigure ==
+                    gameGrid.Children.Cast<GameButton>().First(p => Grid.GetRow(p) == rowI + 1 && Grid.GetColumn(p) == j).CurrentFigure)
+                {
+                    if (tempMatchCoords.Count == 0)
+                    {
+                        tempMatchCoords.Add(new Tuple<int, int>(rowI, j));
+                    }
+                    tempMatchCoords.Add(new Tuple<int, int>(rowI + 1, j));
+                }
+                else if (tempMatchCoords.Count >= MINMATCHCOUNT)
+                {
+                    foreach (var el in tempMatchCoords)
+                        matchCoords.Add(el);
+                    tempMatchCoords.Clear();
+                }
+                else tempMatchCoords.Clear();
+            }
+            if (tempMatchCoords.Count >= MINMATCHCOUNT)
+            {
+                foreach (var el in tempMatchCoords)
+                    matchCoords.Add(el);
+            }
+            return matchCoords;
         }
 
         private void GameWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             Owner.Visibility = Visibility.Visible;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < CELLSCOUNT; i++)
+            {
+                foreach (var pair in CheckLines(i, i))
+                {
+                    gameGrid.Children.Cast<GameButton>()
+                        .First(p => Grid.GetRow(p) == pair.Item1 && Grid.GetColumn(p) == pair.Item2).Background = Brushes.Red;
+                }
+
+            }
         }
     }
 }
